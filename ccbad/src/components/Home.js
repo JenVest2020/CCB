6cb864ad55518b2bbb66a83820716354acb05c9e
JenVest2020 <jenvest2019@gmail.com>
2020-11-12 13:36:45 -0600

    Home component added

---
+++ ccbad/src/components/Home.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Johnny1 from '../images/Johnny1.jpg';
+import Allen2 from '../images/Allen at AmVets.png';
+import LogoOval from '../images/LogoDesign2Oval.png';
+const Home = () => {
+
+    const Johnny = Johnny1;
+    const Allen = Allen2;
+    const Logo1 = LogoOval;
+
+    return (
+        <div>
+            <section className='bottom-home'>
+                <Link href='#' style={{ textDecoration: 'underline', marginRight: '20%', fontSize: '1rem', color: 'blue' }}>You Tube</Link>
+                <img src={Logo1} alt='band logo oval shaped with mountains and green pasture as background musical note as the b letter in boys' style={{ width: '15%' }}></img>
+                <Link href='#' style={{ textDecoration: 'underline', marginLeft: '20%', fontSize: '1rem', color: 'blue' }}>FaceBook</Link>
+                <h1 style={{ padding: '1%' }}>
+                    WELCOME
+                </h1>
+
+
+            </section>
+            <section className='top-home'>
+                <h1>
+                    Classic Country Boys
+                </h1>
+            </section>
+            <section style={{ display: 'flex', justifyContent: 'center' }}>
+                <div className='jldiv'>
+                    <h2 >Johnny Lindsey</h2>
+                    <img src={Johnny} alt='lead singer Johnny sitting at the microphone and playing his guitar' style={{ width: '100%' }} />
+                </div>
+                <div>
+                    <h2>Come See Us</h2>
+                    <h3 style={{ textDecoration: 'underline' }}>Locations</h3>
+                    <div >
+                        <div style={{ display: 'flex', justifyContent: 'space-between', border: '2px ridge #993300' }}>
+                            <p style={{ fontWeight: 'bold' }}> The Melba Theater</p>
+                            <span>300 S. Main St</span>
+                            <br></br><span>De Soto, MO 63020</span>
+                            <p>Saturday Nov. 14, 5:30pm</p>
+                            <a href='www.melbatheater.com'>Melba Theater website</a>
+                        </div>
+                        {/* <div style={{ display: 'flex', justifyContent: 'space-between', border: '2px ridge #993300' }}>
+                            <p style={{ fontWeight: 'bold' }}>Elk's Lodge</p>
+                            <span>300 S. Main St</span>
+                            <span>De Soto, MO 63020</span>
+                        Fridays 4pm to 7pm
+                        <a href='www.melbatheater.com'>Melba Theater website</a>
+                        </div> */}
+                    </div>
+                    <h2>Contact Us</h2>
+                    <p> Allen Vest ph# (513)210-2934</p>
+                    <p>Johnny Lindsey ph# (314)309-4433</p>
+                </div>
+                <div className='avdiv'>
+                    <h2 >Allen Vest</h2>
+                    <img src={Allen} alt='lead guitarist Allen standing at microphone and tuning his guitar' style={{ width: '100%' }} />
+                </div>
+            </section>
+
+
+
+        </div>
+    );
+};
+
+export default Home;
